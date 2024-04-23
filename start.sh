6cf8a328d4ceaae554029c43045495e92ac026ca
Daniel <vukyanh001@gmail.com>
2024-04-23 22:57:56 +0700

    fix issue can not deploy latest image

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@
 set -e
 
 echo "Run the DB migrations"
-#source /app/app.env
+source /app/app.env
 /app/migrate -path /app/migration -database "$DB_SOURCE" -verbose up
 
 echo "Starting the application"
